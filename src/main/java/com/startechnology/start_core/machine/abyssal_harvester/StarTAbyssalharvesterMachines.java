9f143abd51e96ca340d544b5d31dec74e07f186f
KillLaAqua <KillLaAqua@gmail.com>
2025-08-25 11:02:08 -0500

    Cheapens Machine Cost (Multi cost a lot less Mythrollic Drill Casings)

--- src/main/java/com/startechnology/start_core/machine/abyssal_harvester/StarTAbyssalharvesterMachines.java
+++ src/main/java/com/startechnology/start_core/machine/abyssal_harvester/StarTAbyssalharvesterMachines.java
@@ -3,10 +3,13 @@ package com.startechnology.start_core.machine.abyssal_harvester;
 import static com.startechnology.start_core.StarTCore.START_REGISTRATE;
 
 import com.gregtechceu.gtceu.GTCEu;
+import com.gregtechceu.gtceu.api.GTCEuAPI;
 import com.gregtechceu.gtceu.api.machine.MultiblockMachineDefinition;
 import com.gregtechceu.gtceu.api.machine.multiblock.PartAbility;
 import com.gregtechceu.gtceu.api.pattern.FactoryBlockPattern;
 import com.gregtechceu.gtceu.api.pattern.Predicates;
+import com.gregtechceu.gtceu.api.data.chemical.ChemicalHelper;
+import com.gregtechceu.gtceu.api.data.tag.TagPrefix;
 import com.startechnology.start_core.machine.StarTMachineUtils;
 import com.startechnology.start_core.machine.StarTPartAbility;
 import com.startechnology.start_core.recipe.StarTRecipeTypes;
@@ -44,11 +47,11 @@ public class StarTAbyssalharvesterMachines {
             .aisle("             ", "             ", "     DDD     ", "   DD   D    ", "   D         ", "         D   ", "    D   DD   ", "   DD   D    ", "   D         ", "         D   ", "    D   DD   ", "   DD   D    ", "   D         ", "         D   ", "     DDD     ", "             ", "             ")
             .aisle("             ", "     OOO     ", "   DD   DD   ", "             ", "  D       D  ", "  D       D  ", "             ", "             ", "  D       D  ", "  D       D  ", "             ", "             ", "  D       D  ", "  D       D  ", "   DD   DD   ", "     OOO     ", "             ")
             .aisle("             ", "   OOOOOOO   ", "  D  GGB  D  ", "     GGB   D ", "      G    D ", " D           ", " D           ", "           D ", "           D ", " D           ", " D           ", "           D ", "      G    D ", " D   BGG     ", "  D  BGG  D  ", "   OOOOOOO   ", "             ")
-            .aisle("      D      ", "   OOODOOO   ", "  D GGGGG D  ", " D  GGGGB  D ", "    GGGGB    ", "     GGG     ", " D    G    D ", " D         D ", "             ", "             ", " D    G    D ", " D   GGG   D ", "    BGGGG    ", "    BGGGG    ", "  D GGGGG D  ", "   OOODOOO   ", "      D      ")
-            .aisle("     DID     ", "  OOODDDOOO  ", " D GGGGGGG D ", "D  GGGGGGG   ", "D   GGGGB    ", "    GGGGB   D", "     BGG    D", "D    BBG     ", "D     B      ", "     GBB    D", "     GGB    D", "D   BGGGG    ", "D   BGGGG    ", "   GGGGGGG  D", " D GGGGGGG D ", "  OOODDDOOO  ", "     DID     ")
-            .aisle("    DIIID    ", "  OODDPDDOO  ", " D GGGPGGG D ", "   GGGPGGG   ", "D  GGGPGGG  D", "D   BGPGB   D", "    BGPGB    ", "     GPG     ", "D    GPG    D", "D    GPG    D", "    BGPGB    ", "    BGPGB    ", "D  GGGPGGG  D", "D  GGGPGGG  D", " D GGGPGGG D ", "  OOODPDOOO  ", "    DIIID    ")
-            .aisle("     DID     ", "  OOODDDOOO  ", " D GGGGGGG D ", "   GGGGGGG  D", "    BGGGG   D", "D   BGGGG    ", "D    GGB     ", "     GBB    D", "      B     D", "D    BBG     ", "D    BGG     ", "    GGGGB   D", "    GGGGB   D", "D  GGGGGGG   ", " D GGGGGGG D ", "  OOODDDOOO  ", "     DID     ")
-            .aisle("      D      ", "   OOODOOO   ", "  D GGGGG D  ", " g  BGGGG  D ", "    BGGGG    ", "     GGG     ", " D    G    D ", " D         D ", "             ", "             ", " D    G    D ", " D   GGG   D ", "    GGGGB    ", "    GGGGB    ", "  D GGGGG D  ", "   OOODOOO   ", "      D      ")
+            .aisle("      D      ", "   OOODOOO   ", "  D GVVVG D  ", " D  GVVVB  D ", "    GGVGB    ", "     GGG     ", " D    G    D ", " D         D ", "             ", "             ", " D    G    D ", " D   GGG   D ", "    BGVGG    ", "    BVVVG    ", "  D GVVVG D  ", "   OOODOOO   ", "      D      ")
+            .aisle("     DID     ", "  OOODDDOOO  ", " D GVVVVVG D ", "D  GVVVVVG   ", "D   GVVVB    ", "    GVVVB   D", "     BVG    D", "D    BBG     ", "D     B      ", "     GBB    D", "     GVB    D", "D   BVVVG    ", "D   BVVVG    ", "   GVVVVVG  D", " D GVVVVVG D ", "  OOODDDOOO  ", "     DID     ")
+            .aisle("    DIIID    ", "  OODDPDDOO  ", " D GVVPVVG D ", "   GVVPVVG   ", "D  GVVPVVG  D", "D   BVPVB   D", "    BVPVB    ", "     GPG     ", "D    GPG    D", "D    GPG    D", "    BVPVB    ", "    BVPVB    ", "D  GVVPVVG  D", "D  GVVPVVG  D", " D GVVPVVG D ", "  OOODPDOOO  ", "    DIIID    ")
+            .aisle("     DID     ", "  OOODDDOOO  ", " D GVVVVVG D ", "   GVVVVVG  D", "    BVVVG   D", "D   BVVVG    ", "D    GVB     ", "     GBB    D", "      B     D", "D    BBG     ", "D    BVG     ", "    GVVVB   D", "    GVVVB   D", "D  GVVVVVG   ", " D GVVVVVG D ", "  OOODDDOOO  ", "     DID     ")
+            .aisle("      D      ", "   OOODOOO   ", "  D GVVVG D  ", " g  BVVVG  D ", "    BGVGG    ", "     GGG     ", " D    G    D ", " D         D ", "             ", "             ", " D    G    D ", " D   GGG   D ", "    GGVGB    ", "    GVVVB    ", "  D GVVVG D  ", "   OOODOOO   ", "      D      ")
             .aisle("             ", "   OOOOOOO   ", "  D  BGG  D  ", " g   BGG     ", " g    G      ", "           D ", "           D ", " D           ", " D           ", "           D ", "           D ", " D           ", " D    G      ", "     GGB   g ", "  D  GGB  D  ", "   OOOOOOO   ", "             ")
             .aisle("             ", "     OOO     ", "   DD   DD   ", "             ", "  g       D  ", "  g       D  ", "             ", "             ", "  D       D  ", "  D       D  ", "             ", "             ", "  D       g  ", "  D       g  ", "   DD   DD   ", "     OOO     ", "             ")
             .aisle("             ", "             ", "     DDD     ", "    D   DD   ", "         D   ", "   g         ", "   gg   D    ", "    g   DD   ", "         D   ", "   D         ", "   DD   g    ", "    D   gg   ", "         g   ", "   D         ", "     DDD     ", "             ", "             ")
@@ -60,6 +63,7 @@ public class StarTAbyssalharvesterMachines {
             .where('g', Predicates.blocks(StarTMachineUtils.getKjsBlock("draco_resilient_fusion_glass")))
             .where('I', Predicates.blocks(StarTCasingBlocks.ENGINE_INTAKE_ENRICHED_NAQUADAH.get()))
             .where('P', Predicates.blocks(StarTMachineUtils.getKjsBlock("enriched_naquadah_pipe_casing")))
+            .where('V', Predicates.blocks(ChemicalHelper.getBlock(TagPrefix.frameGt, GTCEuAPI.materialManager.getMaterial("gtceu:void"))))
             .where('O', Predicates.blocks(StarTMachineUtils.getKjsBlock("draneko_casing"))
                 .setMinGlobalLimited(80)
                 .or(Predicates.autoAbilities(definition.getRecipeTypes()))
