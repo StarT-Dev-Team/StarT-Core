06e723e253bc9b23649d1fdca6e452b5a8c36ef0
n1xx1 <me@simonemiraglia.it>
2026-02-09 10:52:59 +0100

    Add vacuum pumps, can be defined in kjs or core, run data gen

---
+++ src/main/java/com/startechnology/start_core/block/VacuumPumpBlock.java
@@ -0,0 +1,78 @@
+package com.startechnology.start_core.block;
+
+import com.startechnology.start_core.StarTCore;
+import com.startechnology.start_core.api.vacuumpump.IVacuumPumpType;
+import lombok.Getter;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
+
+@ParametersAreNonnullByDefault
+public class VacuumPumpBlock extends Block {
+
+    public IVacuumPumpType pumpTier;
+
+    public VacuumPumpBlock(Properties properties, IVacuumPumpType pumpTier) {
+        super(properties);
+        this.pumpTier = pumpTier;
+    }
+
+    @Override
+    public BlockState getAppearance(BlockState state, BlockAndTintGetter level, BlockPos pos, Direction side, @Nullable BlockState sourceState, @Nullable BlockPos sourcePos) {
+        return defaultBlockState();
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable BlockGetter level, List<Component> tooltip, TooltipFlag flag) {
+        super.appendHoverText(stack, level, tooltip, flag);
+        // TODO: add language keys
+        tooltip.add(Component.translatable("§cVac% Capacity §f%f%%", pumpTier.getCap()));
+        tooltip.add(Component.translatable("§cVac% Increase Per Second §f%f%%", pumpTier.getRate()));
+    }
+
+    public enum VacuumPumpType implements IVacuumPumpType {
+
+        // TODO: add textures
+        ZPM("zpm", 5f, 80f, StarTCore.resourceLocation("block/pumps/zpm")),
+        UV("uv", 10f, 85f, StarTCore.resourceLocation("block/pumps/uv")),
+        UHV("uhv", 15f, 90f, StarTCore.resourceLocation("block/pumps/uhv")),
+        UEV("uev", 20f, 95f, StarTCore.resourceLocation("block/pumps/uev")),
+        UIV("uiv", 25f, 100f, StarTCore.resourceLocation("block/pumps/uiv"));
+
+        @Getter
+        public final String name;
+
+        @Getter
+        public final float rate;
+
+        @Getter
+        public final float cap;
+
+        @Getter
+        public final ResourceLocation texture;
+
+        VacuumPumpType(String name, float rate, float cap, ResourceLocation texture) {
+            this.name = name;
+            this.rate = rate;
+            this.cap = cap;
+            this.texture = texture;
+        }
+
+        @Override
+        public int getTier() {
+            return this.ordinal();
+        }
+
+    }
+}
