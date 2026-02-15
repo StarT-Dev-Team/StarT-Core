23e8dddfc0907df1a34a0a925f68baf560e27669
stellaurora <jean1398reborn@gmail.com>
2026-02-15 13:33:42 +1100

    indicator record

--- src/main/java/com/startechnology/start_core/api/syncdata/StarTSyncDataAccessors.java
+++ src/main/java/com/startechnology/start_core/api/syncdata/StarTSyncDataAccessors.java
@@ -5,14 +5,18 @@ import static com.lowdragmc.lowdraglib.syncdata.TypedPayloadRegistries.*;
 
 import com.lowdragmc.lowdraglib.syncdata.IAccessor;
 import com.lowdragmc.lowdraglib.syncdata.payload.FriendlyBufPayload;
+import com.startechnology.start_core.machine.redstone.StarTRedstoneIndicatorRecord;
 import com.startechnology.start_core.machine.threading.StarTThreadingCapableMachine;
 
 public class StarTSyncDataAccessors {
 
     public static final IAccessor START_THREADED_RECIPE_ACCESSOR_TYPE = new StarTThreadingCapableMachine.ThreadedRecipeExecutionAccessor();
+    public static final IAccessor START_REDSTONE_INDICATOR_RECORD_ACCESSOR_TYPE = new StarTRedstoneIndicatorRecord.StarTRedstoneIndicatorRecordAccessor();
+
 
     /* Register all the typed paylod registry accessors here */
     public static void init() {
         register(FriendlyBufPayload.class, FriendlyBufPayload::new, START_THREADED_RECIPE_ACCESSOR_TYPE, 100);
+        register(FriendlyBufPayload.class, FriendlyBufPayload::new, START_REDSTONE_INDICATOR_RECORD_ACCESSOR_TYPE, 100);
     }
 }
---
+++ src/main/java/com/startechnology/start_core/machine/redstone/StarTRedstoneIndicatorRecord.java
@@ -0,0 +1,55 @@
+package com.startechnology.start_core.machine.redstone;
+
+import com.lowdragmc.lowdraglib.syncdata.AccessorOp;
+import com.lowdragmc.lowdraglib.syncdata.accessor.CustomObjectAccessor;
+import com.lowdragmc.lowdraglib.syncdata.payload.FriendlyBufPayload;
+import com.lowdragmc.lowdraglib.syncdata.payload.ITypedPayload;
+import io.netty.buffer.Unpooled;
+import net.minecraft.network.FriendlyByteBuf;
+
+public record StarTRedstoneIndicatorRecord(
+        String indicatorKey,
+        String descriptionKey,
+        Integer redstoneLevel,
+        Integer ordering) {
+
+    public static StarTRedstoneIndicatorRecord defaultIndicator() {
+        return new StarTRedstoneIndicatorRecord("variadic.start_core.default",
+                "variadic.start_core.description.default", 0, 0);
+    }
+
+    /* This accessor allows LDLIB to serialize/deserialize over network for the sync annotations */
+    public static class StarTRedstoneIndicatorRecordAccessor
+            extends CustomObjectAccessor<StarTRedstoneIndicatorRecord> {
+
+        public StarTRedstoneIndicatorRecordAccessor() {
+            super(StarTRedstoneIndicatorRecord.class, true);
+        }
+
+        @Override
+        public ITypedPayload<?> serialize(AccessorOp op, StarTRedstoneIndicatorRecord indicatorRecord) {
+            FriendlyByteBuf serializedHolder = new FriendlyByteBuf(Unpooled.buffer());
+
+            serializedHolder.writeUtf(indicatorRecord.indicatorKey);
+            serializedHolder.writeUtf(indicatorRecord.descriptionKey);
+            serializedHolder.writeInt(indicatorRecord.redstoneLevel);
+            serializedHolder.writeInt(indicatorRecord.ordering);
+
+            return FriendlyBufPayload.of(serializedHolder);
+        }
+
+        @Override
+        public StarTRedstoneIndicatorRecord deserialize(AccessorOp op, ITypedPayload<?> payload) {
+            if (payload instanceof FriendlyBufPayload buffer) {
+                String indicatorKey = buffer.getPayload().readUtf();
+                String descriptionKey = buffer.getPayload().readUtf();
+                Integer redstoneLevel = buffer.getPayload().readInt();
+                Integer ordering = buffer.getPayload().readInt();
+
+                return new StarTRedstoneIndicatorRecord(indicatorKey, descriptionKey, redstoneLevel, ordering);
+            }
+            return null;
+        }
+
+    }
+}
