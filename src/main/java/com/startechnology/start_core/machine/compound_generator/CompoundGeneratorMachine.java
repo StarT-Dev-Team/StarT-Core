2576b49cb375277b0f5e446f0788562745f2837b
n1xx1 <me@simonemiraglia.it>
2026-07-28 12:45:29 +0200

    fix compound generator recipe modifier to correctly model how generators behave at higher tiers

--- src/main/java/com/startechnology/start_core/machine/compound_generator/CompoundGeneratorMachine.java
+++ src/main/java/com/startechnology/start_core/machine/compound_generator/CompoundGeneratorMachine.java
@@ -1,11 +1,15 @@
 package com.startechnology.start_core.machine.compound_generator;
 
+import com.gregtechceu.gtceu.api.GTValues;
 import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
 import com.gregtechceu.gtceu.api.machine.MetaMachine;
 import com.gregtechceu.gtceu.api.machine.multiblock.WorkableElectricMultiblockMachine;
 import com.gregtechceu.gtceu.api.recipe.GTRecipe;
+import com.gregtechceu.gtceu.api.recipe.content.ContentModifier;
 import com.gregtechceu.gtceu.api.recipe.modifier.ModifierFunction;
 import com.gregtechceu.gtceu.api.recipe.modifier.ParallelLogic;
+import com.gregtechceu.gtceu.api.recipe.modifier.RecipeModifier;
+import com.gregtechceu.gtceu.common.data.GTParallelTypes;
 import com.gregtechceu.gtceu.common.machine.multiblock.part.EnergyHatchPartMachine;
 import com.startechnology.start_core.recipe.StarTParallelTypes;
 import lombok.Getter;
@@ -13,7 +17,7 @@ import lombok.Getter;
 public class CompoundGeneratorMachine extends WorkableElectricMultiblockMachine {
 
     @Getter
-    private int tier;
+    private final int tier;
 
     @Getter
     private int slices;
@@ -30,20 +34,37 @@ public class CompoundGeneratorMachine extends WorkableElectricMultiblockMachine 
         slices = (int) getParts().stream().filter(EnergyHatchPartMachine.class::isInstance).count();
     }
 
+    @Override
+    public long getOverclockVoltage() {
+        return GTValues.VEX[tier] * 2 * slices;
+    }
+
     public static ModifierFunction recipeModifier(MetaMachine machine, GTRecipe recipe) {
-        if (machine instanceof CompoundGeneratorMachine controller && controller.isFormed()) {
-            int tier = controller.getTier(); // Voltage tier (1, 2 or 3)
-            int slices = controller.getSlices(); // 1 slice = 2 parallels
-            int maxParallels = slices * 2 * (int) Math.pow(4, tier - 1);
-            int parallelsAvailable = ParallelLogic.getParallelAmountWithoutEU(machine, recipe, maxParallels);
-            if (parallelsAvailable > 0) {
-                int parallels = Math.min(parallelsAvailable, maxParallels);
-                return ModifierFunction.builder()
-                        .parallels(parallels, StarTParallelTypes.COMPOUND_GENERATOR)
-                        .eutMultiplier(parallels) // 32 * parallels
-                        .build();
-            }
-        }
-        return ModifierFunction.IDENTITY;
+        if (!(machine instanceof CompoundGeneratorMachine generator) || !generator.isFormed())
+            return RecipeModifier.nullWrongType(CompoundGeneratorMachine.class, machine);
+
+        var EUt = recipe.getOutputEUt().getTotalEU();
+        if (EUt <= 0L)
+            return ModifierFunction.NULL;
+
+        var maxParallels = (int) (generator.getOverclockVoltage() / EUt);
+        var parallels = ParallelLogic.getParallelAmount(machine, recipe, maxParallels);
+
+        if (parallels == 0) return ModifierFunction.NULL;
+
+        var maxTierParallels = (int) (GTValues.VEX[generator.tier] / EUt) * 2;
+        var tierParallels = Math.min(parallels, maxTierParallels);
+        var sliceParallels = parallels / tierParallels;
+        var modifier = ModifierFunction.builder()
+                .modifyAllContents(ContentModifier.multiplier(parallels))
+                .eutMultiplier(parallels);
+
+        if (tierParallels > 1)
+            modifier.parallels(tierParallels, GTParallelTypes.GENERATOR);
+
+        if (sliceParallels > 1)
+            modifier.parallels(sliceParallels, StarTParallelTypes.COMPOUND_GENERATOR);
+
+        return modifier.build();
     }
 }
